a259f2261f29c199462785972b9187849d92139b
faizahaque90 <163949218+faizahaque90@users.noreply.github.com>
2024-06-14 21:25:40 -0700

    added new fuction for the note to stay and go back to the notes if we clicked out of it

--- routes/note.js
+++ routes/note.js
@@ -3,15 +3,36 @@ const express = require("express");
 const fs = require("fs");
 const note = express.Router();
 const file = "./db/db.json";
+let data = "";
 
-note.get("/", async(req, res) => {
-    const data = await getData()
+note.get("/", async (req, res) => {
+    data = await getData();
 
     res.json(JSON.parse(data));
 });
+note.post("/", (req, res) => {
+    let newNote = req.body;
+    const newData = JSON.parse(data);
+    newNote.id = `${newData.length}`;// remove this later
+    newData.push(newNote);
+    data = JSON.stringify(newData);
+    saveNewNote(data);
+    //console.log(message)
+    res.json(newNote);
+})
 const getData = async () => {
     const data = await readFile(file, { encoding: "utf8" });
     //console.log (data);
     return data;
+}
+const saveNewNote = async (data) => {
+    await fs.writeFile(file, data, error => {
+        if (error) {
+            console.log("error occurs while writing file")
+            //return "error occurs while writing file";
+        }
+        //return "new is saved"
+        console.log("new data is saved")
+    })
 }
 module.exports = note;
